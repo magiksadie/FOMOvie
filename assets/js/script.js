bfa24eceac579e07234222eb4c8961194ab78f56
Erock FaQue <eric.kemerer@gmail.com>
2022-02-09 21:07:45 -0500

    added commenting, commented out drag and drop function for future use

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,3 +134,55 @@ function saveMovie(omdbResponse) {
   movieCard.appendChild(moviePoster);
   toWatch.appendChild(movieCard);
 }
+// Drop and Drag Function--
+
+  //class App {
+
+  //static init() {
+
+    //App.box = document.getElementsByClassName('content')[0]
+
+    //App.box.addEventListener("dragstart", App.dragstart)
+    //App.box.addEventListener("dragend", App.dragend)
+
+    //const containers = document.getElementsByClassName('holder')
+
+    //for(const container of containers) {
+      //container.addEventListener("dragover", App.dragover)
+      //container.addEventListener("dragenter", App.dragenter)
+      //container.addEventListener("dragleave", App.dragleave)
+      //container.addEventListener("drop", App.drop)
+    //}
+  //}
+
+  //static dragstart() {
+    //this.className += "held"
+  
+    //setTimeout(()=>this.className="invisible", 0)
+  //}
+
+  //static dragend() {
+    //this.className = "content"
+  //}
+
+  //static dragover(e) {
+    //e.preventDefault()
+  //}
+
+  //static dragenter(e) {
+   // e.preventDefault()
+    //this.className += "hovered"
+  //}
+
+  //static dragleave() {
+    //this.className = "holder"
+  //}
+
+  //static drop() {
+    //this.className = "holder"
+    //this.append(App.box)
+  //}
+
+//}
+
+//document.addEventListener("DOMContentLoaded", App.init)
